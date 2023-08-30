801fe341c268f4d5762fb5b93a7018fdd504a342
Schattsch <karelsch123@gmail.com>
2023-08-30 16:41:24 +0300

    AuthContexti kasutamine

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,15 +1,12 @@
 import React from 'react';
-
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
-
 const MainHeader = (props) => {
-  return (
-    <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
-    </header>
-  );
+    return (
+        <header className={classes['main-header']}>
+            <h1>A Typical Page</h1>
+            <Navigation onLogout={props.onLogout} />
+        </header>
+    );
 };
-
 export default MainHeader;
